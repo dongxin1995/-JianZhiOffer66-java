c67379ac214953136ac315db52e8421aba85ebd2
dongxin1995 <alwaysonlinedx@163.com>
2019-08-14 20:41:18 +0800

    第三次上传剑指Offer27-37道编程题

--- src/com/nercel/Niuke/Solution35.java
+++ src/com/nercel/Niuke/Solution35.java
@@ -17,10 +17,57 @@ package com.nercel.Niuke;
        对于%75的数据,size<=10^5
 
        对于%100的数据,size<=2*10^5
+
+
+https://www.cnblogs.com/coffy/p/5896541.html
  */
 public class Solution35 {
     public int InversePairs(int [] array) {
+        if(array==null||array.length==0)
+        {
+            return 0;
+        }
+        int[] copy = new int[array.length];
+        for(int i=0;i<array.length;i++)
+        {
+            copy[i] = array[i];
+        }
+        int count = InversePairsCore(array,copy,0,array.length-1);//数值过大求余
+        return count;
 
-        return 0;
+    }
+    private  static int InversePairsCore(int[] array,int[] copy,int low,int high) {
+        if (low == high) {
+            return 0;
+        }
+        int mid = (low + high) >> 1;
+        int leftCount = InversePairsCore(array, copy, low, mid) % 1000000007;
+        int rightCount = InversePairsCore(array, copy, mid + 1, high) % 1000000007;
+        int count = 0;
+        int i = mid;
+        int j = high;
+        int locCopy = high;
+        while (i >= low && j > mid) {
+            if (array[i] > array[j]) {
+                count += j - mid;
+                copy[locCopy--] = array[i--];
+                if (count >= 1000000007)//数值过大求余
+                {
+                    count %= 1000000007;
+                }
+            } else {
+                copy[locCopy--] = array[j--];
+            }
+        }
+        for (; i >= low; i--) {
+            copy[locCopy--] = array[i];
+        }
+        for (; j > mid; j--) {
+            copy[locCopy--] = array[j];
+        }
+        for (int s = low; s <= high; s++) {
+            array[s] = copy[s];
+        }
+        return (leftCount + rightCount + count) % 1000000007;
     }
 }
